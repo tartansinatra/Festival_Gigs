7a614e07651f7626a6f8c2333b32bc57a32129b0
Stuart Ramsay <tartansinatra2@yahoo.co.uk>
2016-01-11 17:40:25 +0000

    modified bookings_controller & revise venue index/show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,24 @@
 class BookingsController < ApplicationController
 
-  before_action :authenticate_user! 
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+    
+  def edit
+  end
+
+  def destroy
+  end
+
 
 end
