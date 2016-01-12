570736194f03b743621e0b3b4cd70cfcc1a694f2
Stuart Ramsay <tartansinatra2@yahoo.co.uk>
2016-01-12 17:35:16 +0000

    revised buttons on venue & artist views

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -2,4 +2,27 @@ class Gig < ActiveRecord::Base
   belongs_to :artist
   belongs_to :venue
   has_many :bookings
+
+  validates :start_date, presence: :true 
+  validates :end_date, presence: :true
+  validate :start_date_in_future
+  validate :end_date_is_after_start_date 
+
+
+  private
+
+  def start_date_in_future
+    if start_date.present? && start_date < Time.now
+      errors.add(:start_date, "can't be in the past!")
+    end
+  end  
+
+  def end_date_is_after_start_date
+    return if end_date.blank? || start_date.blank?
+    if end_date < start_date
+      errors.add(:end_date, "can't end a gig after it starts")
+    end
+  end
+    
+
 end
